3f5694c6803b0fb99a061dacd9c313ab29a9a4e5
David Vinson <dvinson@bushelpowered.com>
2021-09-03 11:34:55 -0500

    refactored CustomTypes

---
+++ pokedex/src/CustomTypes/index.ts
@@ -0,0 +1,57 @@
+export interface HeaderPropsInterface {
+    title: string;
+}
+
+export interface PokemonDetailInterface {
+    id: string;
+    name: string;
+    image: string;
+    types: string[];
+    height: string;
+    weight: string;
+    abilities: string[];
+    egg_groups: string[];
+    stats: {
+        hp: string;
+        speed: string;
+        attack: string;
+        defense: string;
+        'special-attack': string;
+        'special-defense': string;
+    };
+    genus: string;
+    description: string;
+}
+
+export interface PokemonListInterface {
+    id: string;
+    image: string;
+    name: string;
+    types: string[];
+}
+
+export interface PokemonLinksInterface {
+    first: string;
+    last: string;
+    prev: null | string;
+    next: string;
+}
+
+export interface PokemonMetaInterface {
+    current_page: string;
+    from: string;
+    last_page: string;
+    path: string;
+    per_page: string;
+    to: string;
+    total: string;
+}
+
+export interface UrlParams {
+    name: string | undefined;
+    pageNum: string;
+}
+
+export interface ApiPropsInterface {
+    pokedexApi: string;
+}
