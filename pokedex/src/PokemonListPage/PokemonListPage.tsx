f6d9729e51e05aeee58d70c849de01d38b7ff36e
David Vinson <dvinson@bushelpowered.com>
2021-09-14 16:45:22 -0500

    refector getPokemonInfo

--- pokedex/src/PokemonListPage/PokemonListPage.tsx
+++ pokedex/src/PokemonListPage/PokemonListPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect, useState, ChangeEvent, KeyboardEvent } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { PokemonListInterface, PokemonDataLinksInterface, UrlParams, PokemonMetaInterface } from 'Types';
+import { UrlParams, ApiDataInterface } from 'Types';
 import { getPokemonInfo, findPokemon, getPage } from 'services/api';
 import { FaSearch, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import {
@@ -25,29 +25,21 @@ import { MessageBox } from 'styleComps';
 function PokemonListPage() {
     const history = useHistory();
     const params: UrlParams = useParams();
-    const [pokemonList, setPokemonList] = useState<PokemonListInterface[]>();
-    const [pokemonDataLinks, setPokemonDataLinks] = useState<PokemonDataLinksInterface>();
-    const [pokemonMeta, setPokemonMeta] = useState<PokemonMetaInterface>();
     const [startPage] = useState('1');
     const [pokemonNameSearch, setPokemonNameSearch] = useState('');
     const [isLargerThan480] = useMediaQuery('(min-width: 480px)');
+    const [pokemonInfo, setPokemonInfo] = useState<ApiDataInterface>();
 
     useEffect(() => {
         getPokemonInfo(params).then((response) => {
-            setPokemonList(response.data.data);
-            setPokemonDataLinks(response.data.links);
-            setPokemonMeta(response.data.meta);
+            setPokemonInfo(response.data);
         });
     }, []);
 
-    const pageNav = (pagination: string | undefined) => {
-        const page = String(pagination);
-        const strIndex = page.indexOf('=');
-        const pageNum = page.slice(strIndex + 1);
+    const pageNav = (page: string) => {
+        const pageNum = page?.slice(page?.indexOf('=') + 1);
         getPage(page, pageNum, pokemonNameSearch).then((response) => {
-            setPokemonList(response.data.data);
-            setPokemonDataLinks(response.data.links);
-            setPokemonMeta(response.data.meta);
+            setPokemonInfo(response.data);
             history.push(`/page/${pageNum}`);
         });
     };
@@ -59,10 +51,7 @@ function PokemonListPage() {
     const submitSearch = (event: KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
             findPokemon(pokemonNameSearch).then((response) => {
-                setPokemonList(response.data.data);
-                setPokemonDataLinks(response.data.links);
-                setPokemonMeta(response.data.meta);
-
+                setPokemonInfo(response.data);
                 history.push(`/page/${startPage}`);
             });
         }
@@ -71,9 +60,7 @@ function PokemonListPage() {
     const onCloseSearch = () => {
         setPokemonNameSearch('');
         getPokemonInfo(params).then((response) => {
-            setPokemonList(response.data.data);
-            setPokemonDataLinks(response.data.links);
-            setPokemonMeta(response.data.meta);
+            setPokemonInfo(response.data);
         });
     };
 
@@ -88,7 +75,7 @@ function PokemonListPage() {
                             isRound={true}
                             size="lg"
                             bgColor="teal.500"
-                            onClick={() => pageNav(pokemonDataLinks?.prev)}
+                            onClick={() => pageNav(pokemonInfo?.links.prev ?? 'null')}
                         />
                     ) : (
                         <IconButton aria-label="left-arrow" size="lg" disabled={true} visibility="hidden" />
@@ -120,14 +107,14 @@ function PokemonListPage() {
                         </InputRightElement>
                     </FormControl>
 
-                    {pokemonMeta?.current_page !== pokemonMeta?.last_page ? (
+                    {pokemonInfo?.meta.current_page !== pokemonInfo?.meta.last_page ? (
                         <IconButton
                             aria-label="right-arrow"
                             icon={<FaArrowRight color="white" />}
                             isRound={true}
                             size="lg"
                             bgColor="teal.500"
-                            onClick={() => pageNav(pokemonDataLinks?.next)}
+                            onClick={() => pageNav(pokemonInfo?.links.next ?? 'null')}
                         />
                     ) : (
                         <IconButton aria-label="left-arrow" size="lg" disabled={true} visibility="hidden" />
@@ -142,7 +129,7 @@ function PokemonListPage() {
                             isRound={true}
                             size="lg"
                             bgColor="teal.500"
-                            onClick={() => pageNav(pokemonDataLinks?.prev)}
+                            onClick={() => pageNav(pokemonInfo?.links.prev ?? 'null')}
                         />
                     ) : (
                         <IconButton aria-label="left-arrow" size="lg" disabled={true} visibility="hidden" />
@@ -167,14 +154,14 @@ function PokemonListPage() {
                         </InputRightElement>
                     </FormControl>
 
-                    {pokemonMeta?.current_page !== pokemonMeta?.last_page ? (
+                    {pokemonInfo?.meta.current_page !== pokemonInfo?.meta.last_page ? (
                         <IconButton
                             aria-label="right-arrow"
                             icon={<FaArrowRight color="white" />}
                             isRound={true}
                             size="lg"
                             bgColor="teal.500"
-                            onClick={() => pageNav(pokemonDataLinks?.next)}
+                            onClick={() => pageNav(pokemonInfo?.links.next ?? 'null')}
                         />
                     ) : (
                         <IconButton aria-label="left-arrow" size="lg" disabled={true} visibility="hidden" />
@@ -183,10 +170,10 @@ function PokemonListPage() {
             )}
 
             <SimpleGrid columns={{ base: 2, md: 3 }} spacing={4}>
-                {pokemonList?.length === 0 ? (
+                {pokemonInfo?.data.length === 0 ? (
                     <MessageBox>Oops! No Pokemon Found</MessageBox>
                 ) : (
-                    pokemonList?.map((poke) => (
+                    pokemonInfo?.data.map((poke) => (
                         <Box
                             key={poke.id}
                             cursor="pointer"
@@ -197,7 +184,7 @@ function PokemonListPage() {
                             whileHover={{ scale: 1.1 }}
                             bg="white"
                             borderRadius="sm"
-                            onClick={() => history.push(`/detail/${pokemonMeta?.current_page}/${poke.id}`)}
+                            onClick={() => history.push(`/detail/${pokemonInfo?.meta.current_page}/${poke.id}`)}
                         >
                             <Box textAlign="left" padding="5px" fontWeight="bold">
                                 {poke.name}
