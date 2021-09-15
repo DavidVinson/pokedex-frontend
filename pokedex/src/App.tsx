f278bc88a7cc083b122137db296d2d673121f110
David Vinson <dvinson@bushelpowered.com>
2021-09-15 11:13:15 -0500

    navigate btns display on small pokeList page

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PokemonListPage from './PokemonListPage/PokemonListPage';
+import PokemonListPageSmall from 'PokemonListPage/PokemonListPageSmall';
 import PokemonDetailPage from './PokemonDetailPage/PokemonDetailPage';
 import PokemonDetailPageSmall from 'PokemonDetailPage/PokemonDetailPageSmall';
 import { Box, useMediaQuery } from '@chakra-ui/react';
@@ -15,7 +16,7 @@ function App() {
                     <Redirect exact from="/" to="/page/1" />
 
                     <Route path="/page/:pageNum">
-                        <PokemonListPage />
+                        {isSmallerThan480 ? <PokemonListPageSmall /> : <PokemonListPage />}
                     </Route>
 
                     <Route path="/detail/:currentPage/:pokeID">
